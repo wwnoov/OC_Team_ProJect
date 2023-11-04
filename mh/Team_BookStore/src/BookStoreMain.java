4056bedf4339b24513d60f9966b04b9f5906eae1
DESKTOP-TTK972Q\angus <leemh9256@gmail.com>
2023-11-04 23:57:27 +0900

    출력 꾸미기 및 자잘한 버그 수정

--- mh/Team_BookStore/src/BookStoreMain.java
+++ mh/Team_BookStore/src/BookStoreMain.java
@@ -2,6 +2,35 @@
 public class BookStoreMain {
 	
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("                /$$$$$$$   /$$$$$$   /$$$$$$  /$$   /$$");
+        System.out.println("               | $$__  $$ /$$__  $$ /$$__  $$| $$  /$$/");
+        System.out.println("               | $$  \\ $$| $$  \\ $$| $$  \\ $$| $$ /$$/");
+        System.out.println("               | $$$$$$$ | $$  | $$| $$  | $$| $$$$$/");
+        System.out.println("               | $$__  $$| $$  | $$| $$  | $$| $$  $$");
+        System.out.println("               | $$  \\ $$| $$  | $$| $$  | $$| $$\\  $$");
+        System.out.println("               | $$$$$$$/|  $$$$$$/|  $$$$$$/| $$ \\  $$");
+        System.out.println("               |_______/  \\______/  \\______/ |__/  \\__/");
+        System.out.println();
+        try {
+            Thread.sleep(700);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("             /$$$$$$  /$$$$$$$$ /$$$$$$  /$$$$$$$  /$$$$$$$$");
+        System.out.println("            /$$__  $$|__  $$__//$$__  $$| $$__  $$| $$_____/");
+        System.out.println("           | $$  \\__/   | $$  | $$  \\ $$| $$  \\ $$| $$");
+        System.out.println("           |  $$$$$$    | $$  | $$  | $$| $$$$$$$/| $$$$$");
+        System.out.println("            \\____  $$   | $$  | $$  | $$| $$__  $$| $$__/");
+        System.out.println("            /$$  \\ $$   | $$  | $$  | $$| $$  \\ $$| $$");
+        System.out.println("           |  $$$$$$/   | $$  |  $$$$$$/| $$  | $$| $$$$$$$$");
+        System.out.println("            \\______/    |__/   \\______/ |__/  |__/|________/");
+        try {
+            Thread.sleep(1500);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println();
         BookStore bookStore = new BookStore();
         bookStore.start();
     }
